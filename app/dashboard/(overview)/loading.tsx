8d0af210960dcd8aa4ba1acbee5c5f6179097b73
d-beloved <morayodeji@gmail.com>
2024-07-12 20:11:00 +0100

    add auth to app, signin and signout

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,5 +1,7 @@
 import DashboardSkeleton from '@/app/ui/skeletons';
 
+// Placed this file and the page.tsx inside the (overview) folder so that the loading state can only
+// affect the main dashboard page and not the invoice and customers pages
 // using this if I want to stream the whole dashboard at once
 // using <Suspense> in the dashboard/page.tsx to stream components independently
 export default function Loading() {
